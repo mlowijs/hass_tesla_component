7d07f11f7f35f491251c10651e60f6534259fd53
Michiel Lowijs <michiel.lowijs@sycade.com>
2018-09-24 22:59:55 +0200

    initial

---
+++ tesla.py
@@ -0,0 +1,67 @@
+"""
+Support for Tesla cars.
+
+For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/tesla/
+"""
+import logging
+
+import voluptuous as vol
+
+from homeassistant.const import (
+    ATTR_BATTERY_LEVEL, CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_USERNAME)
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import discovery
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import slugify
+
+REQUIREMENTS = ['tesla_api==1.0.2']
+
+DOMAIN = 'tesla'
+
+_LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_SCAN_INTERVAL, default=300):
+            vol.All(cv.positive_int, vol.Clamp(min=300)),
+    }),
+}, extra=vol.ALLOW_EXTRA)
+
+TESLA_COMPONENTS = [
+    'climate'
+]
+
+
+def setup(hass, base_config):
+    """Set up of Tesla component."""
+    from tesla_api import TeslaApiClient, AuthenticationError, ApiError
+
+    config = base_config.get(DOMAIN)
+
+    email = config.get(CONF_USERNAME)
+    password = config.get(CONF_PASSWORD)
+
+    api_client = TeslaApiClient(email, password)
+
+    try:    
+        api_client.authenticate()
+
+        hass.data[DOMAIN] = {
+            'vehicles': api_client.vehicles.list()
+        }
+
+        _LOGGER.debug("Connected to the Tesla API.")
+    except AuthenticationError as ex:            
+        _LOGGER.error(ex.message)
+        return False
+    except ApiError as ex:
+        _LOGGER.error(ex.message)
+        return False
+
+    for component in TESLA_COMPONENTS:
+        discovery.load_platform(hass, component, DOMAIN, hass_config=config)
+
+    return True
