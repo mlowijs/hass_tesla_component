0034176b1724bbd81595661e97bbbfd1748db62d
Michiel Lowijs <michiel.lowijs@sycade.com>
2018-09-28 16:38:38 +0200

    stuff

--- climate/tesla.py
+++ climate/tesla.py
@@ -1,2 +1,88 @@
-def setup_platform():
-    pass
+"""
+Support for Tesla HVAC system.
+For more details about this platform, please refer to the documentation
+https://home-assistant.io/components/climate.tesla/
+"""
+from datetime import timedelta
+import logging
+
+from homeassistant.components.climate import (
+    ClimateDevice, SUPPORT_ON_OFF, SUPPORT_TARGET_TEMPERATURE)
+from custom_components.tesla import (
+    DATA_MANAGER, DOMAIN, PLATFORM_ID, VEHICLE_UPDATED, VEHICLES)
+from homeassistant.const import (TEMP_CELSIUS, TEMP_FAHRENHEIT)
+from homeassistant.helpers.event import track_point_in_utc_time
+from homeassistant.util import dt as dt_util
+
+_LOGGER = logging.getLogger(__name__)
+
+DEPENDENCIES = ['tesla']
+
+SUPPORT_FLAGS = SUPPORT_ON_OFF | SUPPORT_TARGET_TEMPERATURE
+
+def setup_platform(hass, config, add_entities, discovery_info):
+    """Set up the Tesla climate platform."""
+    tesla_data = hass.data[DOMAIN]
+
+    climate_devices = [TeslaClimateDevice(hass, vehicle, tesla_data[DATA_MANAGER])
+                       for vehicle in tesla_data[VEHICLES]]
+
+    add_entities(climate_devices, True)
+
+class TeslaClimateDevice(ClimateDevice):
+    def __init__(self, hass, vehicle, data_manager):
+        self._vehicle = vehicle
+        self._data_manager = data_manager
+        self._data = None
+
+        hass.bus.listen(VEHICLE_UPDATED, self.vehicle_updated)
+
+        _LOGGER.debug('Created climate device for {}.'.format(vehicle.vin))
+
+    def vehicle_updated(self, event):
+        if event.data.get('vin') != self._vehicle.vin:
+            return
+
+        self.update()
+
+    def update(self):
+        self._data = self._data_manager.data[self._vehicle.vin]
+        _LOGGER.debug('Updated climate device for {}.'.format(self._vehicle.vin))
+
+    def turn_on(self):
+        self._vehicle.wake_up()
+        self._vehicle.climate.start_climate()
+        self._data_manager.update_vehicle(self._vehicle)
+
+        _LOGGER.debug('Turned climate on for {}.'.format(self._vehicle.vin))
+
+    def turn_off(self):
+        self._vehicle.wake_up()
+        self._vehicle.climate.stop_climate()
+        self._data_manager.update_vehicle(self._vehicle)
+
+        _LOGGER.debug('Turned climate off for {}.'.format(self._vehicle.vin))
+
+    @property
+    def name(self):
+        return PLATFORM_ID.format(self._vehicle.vin)
+
+    @property
+    def supported_features(self):
+        return SUPPORT_FLAGS
+
+    @property
+    def temperature_unit(self):
+        return TEMP_CELSIUS if self._data['gui']['gui_temperature_units'] == 'C' else TEMP_FAHRENHEIT
+
+    @property
+    def is_on(self):
+        return self._data['climate']['is_climate_on']
+
+    @property
+    def current_temperature(self):
+        return self._data['climate']['inside_temp']
+
+    @property
+    def target_temperature(self):
+        return self._data['climate']['driver_temp_setting']
--- tesla.py
+++ tesla.py
@@ -13,9 +13,13 @@ from homeassistant.const import (
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import discovery
 
-REQUIREMENTS = ['tesla_api==1.0.4']
+REQUIREMENTS = ['tesla_api==1.0.5']
 
+DATA_MANAGER = 'data_manager'
 DOMAIN = 'tesla'
+PLATFORM_ID = 'tesla_{}'
+VEHICLE_UPDATED = 'tesla_vehicle_updated'
+VEHICLES = 'vehicles'
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,12 +28,12 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_USERNAME): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
         vol.Optional(CONF_SCAN_INTERVAL, default=300):
-            vol.All(cv.positive_int, vol.Clamp(min=300)),
+            vol.All(cv.positive_int, vol.Clamp(min=10)),
     }),
 }, extra=vol.ALLOW_EXTRA)
 
-TESLA_COMPONENTS = [
-    # 'climate'
+TESLA_PLATFORMS = [
+    'climate'
 ]
 
 def setup(hass, base_config):
@@ -45,9 +49,11 @@ def setup(hass, base_config):
 
     try:
         vehicles = api_client.list_vehicles()
+        data_manager = TeslaDataManager(hass, vehicles)
 
         hass.data[DOMAIN] = {
-            'vehicles': vehicles
+            VEHICLES: vehicles,
+            DATA_MANAGER: data_manager
         }
 
         _LOGGER.debug('Connected to the Tesla API, found {} vehicles.'.format(len(vehicles)))
@@ -58,7 +64,39 @@ def setup(hass, base_config):
         _LOGGER.error(ex.message)
         return False
 
-    for component in TESLA_COMPONENTS:
-        discovery.load_platform(hass, component, DOMAIN, hass_config=config)
+    for platform in TESLA_PLATFORMS:
+        discovery.load_platform(hass, platform, DOMAIN, discovered={CONF_SCAN_INTERVAL: config.get(CONF_SCAN_INTERVAL)})
 
     return True
+
+class TeslaDataManager:
+    def __init__(self, hass, vehicles):
+        self._hass = hass
+        self._vehicles = vehicles
+        self._data = {}
+
+        self.update()
+
+    def update(self):
+        for vehicle in self._vehicles:
+            vehicle.wake_up()
+            self.update_vehicle(vehicle)
+
+    def update_vehicle(self, vehicle):
+        data = {
+            'charge': vehicle.charge.get_state(),
+            'climate': vehicle.climate.get_state(),
+            'drive': vehicle.get_drive_state(),
+            'gui': vehicle.get_gui_settings(),
+            'vehicle': vehicle.get_state()
+        }
+        
+        self._data[vehicle.vin] = data
+        self._hass.bus.fire(VEHICLE_UPDATED, {'vin': vehicle.vin})
+
+        _LOGGER.debug('Updated data for {}'.format(vehicle.vin))
+
+    @property
+    def data(self):
+        return self._data
+        
